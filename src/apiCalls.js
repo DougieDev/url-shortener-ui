e345e8d60d68244afba8809309d2e7f0bfba8a0c
Carlyle Douglas <cdouglascorp@gmail.com>
2020-11-10 10:19:08 -0700

    Add postURL method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,18 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (url, title) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      long_url: url,
+      title: title
+    })
+
+  }).then(response => response.json())
+
+}
